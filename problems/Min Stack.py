676bf479adb05541a5c52b314c20cf13cda0a579
rahulmansharamani14 <rahul2002aug@gmail.com>
2025-09-07 22:45:26 -0500

    Min Stack - 2 stacks

---
+++ problems/Min Stack.py
@@ -0,0 +1,68 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# Implement the MinStack class:
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+
+
+
+# Example 1:
+
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+
+# Output
+# [null,null,null,null,-3,null,0,-2]
+
+# Explanation
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin(); // return -3
+# minStack.pop();
+# minStack.top();    // return 0
+# minStack.getMin(); // return -2
+
+
+# Constraints:
+
+# -231 <= val <= 231 - 1
+# Methods pop, top and getMin operations will always be called on non-empty stacks.
+# At most 3 * 104 calls will be made to push, pop, top, and getMin.
+#
+#
+# Approach:
+# We can maitain two stacks: one normal stack and other to stack to store the minimum element. Every time we push new elements to the stack, we would compare it with the top of the min stack and add the new minimum. Everytime we pop any element from the stack, we also pop it form our minimum stack.
+#
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+
+    def pop(self) -> None:
+        if self.stack:
+            val = self.stack.pop()
+            if val == self.minStack[-1]:
+                self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
