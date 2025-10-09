1758878a4c35987655cdef22e86386d01aa986f7
rahulmansharamani14 <rahul2002aug@gmail.com>
2025-10-08 22:05:25 -0500

    Optimized approach with one pass

--- problems/Minimum Absolute Difference.py
+++ problems/Minimum Absolute Difference.py
@@ -24,7 +24,7 @@
 
 '''
 Approach 1: Sorting the input, computing min_difference in one pass and return all the pairs of absolute difference equal to min_difference
-TC: O(nlogn + 2n)
+TC: O(nlogn + 2n) ~ O(nlogn)
 SC: O(1)
 '''
 
@@ -49,25 +49,55 @@ SC: O(1)
 Approach 2: Sorting the input and Using a Hashmap to store all pairs with thier absolute difference
 '''
 
-def minAbsDifference(arr: list[int]) -> list[list[int]]:
-    arr.sort()
+# def minAbsDifference(arr: list[int]) -> list[list[int]]:
+#     arr.sort()
 
-    diff_map = dict() 
+#     diff_map = dict() 
+
+#     for i in range(len(arr) - 1):
+#         diff = arr[i+1] - arr[i]
+#         if diff in diff_map:
+#             diff_map[diff].append([arr[i], arr[i+1]])
+#         else:
+#             diff_map[diff] = [[arr[i], arr[i+1]]]
+    
+#     min_key = float('inf')
+
+#     for key in diff_map:
+#         min_key = min(min_key, key)
+
+#     return diff_map[min_key]
+    
+
+
+
+
+'''
+Approach 3: One Pass Solution: Compute 1st pair absolute difference and check if any future aboslute diff is less than this, start the res list from that pair
+TC: O(nlogn + n)
+SC; O(1)
+
+'''
+
+def minAbsDifference(arr: list[int]) -> list[list[int]]:
+    arr.sort() #[1,2,3,4]
+
+    min_difference = arr[1] - arr[0]
+
+    res = []
 
     for i in range(len(arr) - 1):
-        diff = arr[i+1] - arr[i]
-        if diff in diff_map:
-            diff_map[diff].append([arr[i], arr[i+1]])
-        else:
-            diff_map[diff] = [[arr[i], arr[i+1]]]
-    
-    min_key = float('inf')
+        if min_difference == arr[i + 1] - arr[i]:
+            res.append([arr[i], arr[i+1]])
+        
+        if min_difference > arr[i + 1] - arr[i]:
+            min_difference = arr[i + 1] - arr[i]
+            res = [[arr[i], arr[i+1]]]
 
-    for key in diff_map:
-        min_key = min(min_key, key)
 
-    return diff_map[min_key]
-    
+    return res
+
+
 
 arr1 = [4,2,1,3]
 print(minAbsDifference(arr1))
@@ -75,3 +105,5 @@ arr2 = [1,3,6,10,15]
 print(minAbsDifference(arr2))
 arr3 = [3,8,-10,23,19,-4,-14,27]
 print(minAbsDifference(arr3))
+arr4 = [1,5,6,8]
+print(minAbsDifference(arr4))
