01ae8c14d06886ae263701fe105939da2c189eca
rahulmansharamani14 <rahul2002aug@gmail.com>
2026-04-21 22:23:38 -0500

    Using DP (Bottom Up) with Tabulation

---
+++ problems/Min Cost Climbing Stairs.py
@@ -0,0 +1,63 @@
+#You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+#
+#You can either start from the step with index 0, or the step with index 1.
+#
+#Return the minimum cost to reach the top of the floor.
+#
+# 
+#
+#Example 1:
+#
+#Input: cost = [10,15,20]
+#Output: 15
+#Explanation: You will start at index 1.
+#- Pay 15 and climb two steps to reach the top.
+#The total cost is 15.
+#Example 2:
+#
+#Input: cost = [1,100,1,1,1,100,1,1,100,1]
+#Output: 6
+#Explanation: You will start at index 0.
+#- Pay 1 and climb two steps to reach index 2.
+#- Pay 1 and climb two steps to reach index 4.
+#- Pay 1 and climb two steps to reach index 6.
+#- Pay 1 and climb one step to reach index 7.
+#- Pay 1 and climb two steps to reach index 9.
+#- Pay 1 and climb one step to reach the top.
+#The total cost is 6.
+# 
+#
+#Constraints:
+#
+#2 <= cost.length <= 1000
+#0 <= cost[i] <= 999
+
+"""
+Approach: DP with bottom Up (Tabulation)
+
+Key idea: To reach the top of the floor (meaning beyond the array), we simply look for the minimum value of it's last two steps cost
+
+TC: O(n)
+SC: O(n) for min_cost array (dp array)
+"""
+
+
+
+def minCostClimbingStairs(cost: list[list[int]]) -> int:
+
+	#base cases
+	n = len(cost)
+	
+	min_cost = [0] * n
+	
+	#initialize base values
+	min_cost[0], min_cost[1] = cost[0], cost[1]
+	
+	#general case
+	for i in range(2, n):
+		min_cost[i] = min(min_cost[i - 1], min_cost[i - 2]) + cost[i]
+	
+	return min(min_cost[n - 1], min_cost[n - 2])
+
+
+
